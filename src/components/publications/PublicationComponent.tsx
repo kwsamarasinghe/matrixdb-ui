ce6f5b608ab1f04e58abda1dfdc6c5af7d6c73fe
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2024-04-23 12:16:26 +0200

    Fix table overflow issue in publication page

--- src/components/publications/PublicationComponent.tsx
+++ src/components/publications/PublicationComponent.tsx
@@ -271,7 +271,7 @@ function PublicationComponent() {
                                                 display: 'flex',
                                                 justifyContent: 'center',
                                                 alignItems: 'center',
-                                                width: '1100px',
+                                                width: '100%',
                                                 margin: '0 auto',
                                                 border: '1px solid #ccc',
                                             }}>
