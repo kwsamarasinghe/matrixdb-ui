c4085c4ef0d35bf8ea69a17f0391f3064e5528c4
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2024-09-06 12:57:33 +0200

    Update biomolecule page

--- src/components/help/BiomoleculeHelp.tsx
+++ src/components/help/BiomoleculeHelp.tsx
@@ -1,4 +1,4 @@
-import {Typography} from "@mui/material";
+import {Box, Divider, List, ListItem, ListItemText, Typography} from "@mui/material";
 import React from "react";
 import CircleIcon from "@mui/icons-material/Circle";
 
@@ -7,135 +7,122 @@ import searchResult from "../../assets/images/help/search-results.png";
 
 function BiomoleculeHelp() {
     return(
-        <div style={{
-            height: '100vh',
-            overflowY: 'auto'
-        }}>
-                <Typography
-                    variant="h6"
-                    style={{
-                        fontWeight: "bold",
-                        paddingTop: "4px"
-                    }}
-                >
+            <Box sx={{ my: 4, paddingBottom: '20px' }}>
+                <Typography variant="h6" gutterBottom>
+                    Overview of Sections
+                </Typography>
+                <Typography variant="body1" paragraph>
+                    A biomolecule page is organized into five sections, each directly accessible via a menu on the top left. If no information is available for a section, the name of that section will not appear in the top left menu. The sections are:
+                </Typography>
+                <List>
+                    <ListItem>
+                        <ListItemText primary="1. Overview" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="2. Interactions" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="3. Expression" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="4. Structure" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="5. Annotations" />
+                    </ListItem>
+                </List>
+
+                <Divider />
+
+                <Typography variant="h6" gutterBottom sx={{ marginTop: 2 }}>
                     Overview Section
                 </Typography>
-                <Typography
-                    variant="body2"
-                    style={{
-                        paddingTop: "4px"
-                    }}
-                >
-                    Overview section provides the following main information;
-                    <br/>
-                    <CircleIcon style={{
-                        fontSize: "0.6em",
-                        paddingRight: "4px"
-                    }}/>
-                    Names and identifiers
-                    <br/>
-                    <CircleIcon style={{
-                        fontSize: "0.6em",
-                        paddingRight: "4px"
-                    }}/>
-                    Biological functions for proteins (source uniprot)
-                    <br/>
-                    <CircleIcon style={{
-                        fontSize: "0.6em",
-                        paddingRight: "4px"
-                    }}/>
-                    Domain annotations for proteins (source uniprot)
-                    <br/>
-                    <CircleIcon style={{
-                        fontSize: "0.6em",
-                        paddingRight: "4px"
-                    }}/>
-                    Cross references for proteins to uniprot and pfrag entry, GAGs, SPEPs, Lipids and Cations chebi identifier, multimers complex portal
-                    <br/>
-                    <CircleIcon style={{
-                        fontSize: "0.6em",
-                        paddingRight: "4px"
-                    }}/>
-                    ECMness label is displayed for proteins, based on the ECMness criterion of matrixdb and matrisome respectively
-                    <span style={{
-                        paddingLeft: "10px"
-                    }}>
-                <img src={ecmness} width="200px" alt="ecmness"/>
-            </span>
+                <Typography variant="body1" paragraph>
+                    The overview section provides the name(s) and identifier of the biomolecule. Further information is available in several tabs, shown only if information is available.
                 </Typography>
 
-                <Typography
-                    variant="h6"
-                    style={{
-                        fontWeight: "bold",
-                        paddingTop: "4px"
-                    }}
-                >
+                <Typography variant="subtitle1" gutterBottom>
+                    If the biomolecule is a protein:
+                </Typography>
+                <ul>
+                    <li>A description of the biological function(s) (source: UniProt)</li>
+                    <li>Details of cellular location (source: UniProt)</li>
+                    <li>A list of protein family/domain annotation (source: UniProt/InterPro)</li>
+                    <li>Cross-references to UniProt and possible PFRAG related entries</li>
+                </ul>
+
+                <Typography variant="subtitle1" gutterBottom>
+                    If the biomolecule is a multimer:
+                </Typography>
+                <ul>
+                    <li>A description and number of assembled chains (source: Complex Portal)</li>
+                    <li>Details of the biological function(s) (source: Complex Portal)</li>
+                    <li>Cross-references to Complex Portal</li>
+                </ul>
+
+                <Typography variant="subtitle1" gutterBottom>
+                    If the biomolecule is a glycosaminoglycan:
+                </Typography>
+                <ul>
+                    <li>A description of the structure (source: ChEBI)</li>
+                    <li>In-house annotation of the GAG sequence</li>
+                    <li>Details of cellular location (source: ?)</li>
+                    <li>A depiction of the molecule in the <a href="https://www.ncbi.nlm.nih.gov/glycans/snfg.html" target="_blank">Symbol Nomenclature for Glycans (SNFG)</a></li>
+                    <li>The GlycoCT encoding of the GAG structure</li>
+                    <li>Cross-references to GlyTouCan and ChEBI</li>
+                </ul>
+
+                <Typography variant="subtitle1" gutterBottom>
+                    If the biomolecule is a cation, a small molecule, or a synthetic peptide:
+                </Typography>
+                <ul>
+                    <li>Cross-references to ChEBI</li>
+                </ul>
+
+                <Typography variant="body1" paragraph>
+                    ECMness label is displayed in the top right banner for proteins, based on the ECMness criterion of MatrixDB ('MatrixDB ECM' blue capsule) and MatrisomeDB categories and subcategories.
+                </Typography>
+
+                <Divider />
+
+                <Typography variant="h6" gutterBottom sx={{ marginTop: 2 }}>
                     Interactions Section
                 </Typography>
-                <Typography
-                    variant="body2"
-                    style={{
-                        paddingTop: "4px"
-                    }}
-                >
-                    Interaction section provides the biomolecule interactions, displayed in a list view and network visualization view with cytoscape.
+                <Typography variant="body1" paragraph>
+                    The 'Interactions' section shows the full set of biomolecules interacting with the biomolecule described above. Information is summarized with three figures:
+                </Typography>
+                <ul>
+                    <li>Number of Participants: counts biomolecules interacting with the subject biomolecule (including self-interactions)</li>
+                    <li>Number of Experimentally Supported Interactions: counts interactions with experimental evidence</li>
+                    <li>Number of Predicted Interactions: counts predicted interactions</li>
+                </ul>
+
+                <Typography variant="body1" paragraph>
+                    Two views are available: Interaction list view and Network view. In the Interaction list view, binary interactions are displayed in a list. In the Network view, a network is visualized using <a href="https://cytoscape.org" target="_blank">Cytoscape</a>.
                 </Typography>
 
-                <Typography
-                    variant="h6"
-                    style={{
-                        fontWeight: "bold",
-                        paddingTop: "4px"
-                    }}
-                >
-                    Transcriptomics and Proteomics Section
+                <Typography variant="subtitle1" gutterBottom>
+                    Interaction List View
                 </Typography>
-                <Typography
-                    variant="body2"
-                    style={{
-                        paddingTop: "4px"
-                    }}
-                >
-                    For proteins this section provides the transcriptomics expression data from bgee and proteomics data from matrisome porject.
+                <Typography variant="body1" paragraph>
+                    Binary interactions are shown in an interactive table with six columns: Participant, Interactions, Non-expanded, Spoke Expanded, MI score, and Type. The table can be exported as a .csv file.
                 </Typography>
 
-                <Typography
-                    variant="h6"
-                    style={{
-                        fontWeight: "bold",
-                        paddingTop: "4px"
-                    }}
-                >
-                    Structure Section
-                </Typography>
-                <Typography
-                    variant="body2"
-                    style={{
-                        paddingTop: "4px"
-                    }}
-                >
-                    For proteins displays the pdb structures in a 3D view using the mol-* visualization library.
+                <Typography variant="body1" paragraph>
+                    Direct interactions, physical associations, and associations are defined according to the <a href="https://www.ebi.ac.uk/intact/documentation/user-guide" target="_blank">IntAct user guide</a>.
                 </Typography>
 
-                <Typography
-                    variant="h6"
-                    style={{
-                        fontWeight: "bold",
-                        paddingTop: "4px"
-                    }}
-                >
-                    Annotation Section
+                <Typography variant="subtitle1" gutterBottom>
+                    Network View
                 </Typography>
-                <Typography
-                    variant="body2"
-                    style={{
-                        paddingTop: "4px"
-                    }}
-                >
-                    For proteins and complexes corresponding GO terms, Uniprot keywords and Rectome data is displayed in this section with their cross references.
+                <Typography variant="body1" paragraph>
+                    All listed binary interactions are mapped to an interactive graph with nodes as biomolecules and edges as interactions. Nodes can be moved and edges adjusted interactively. For more details, refer to the <a href="https://cytoscape.org/documentation_users.html" target="_blank">Cytoscape documentation</a>.
                 </Typography>
-        </div>
+
+                <Typography variant="body1" paragraph>
+                    The graph can be exported as a Cytoscape file or as an image.
+                </Typography>
+            </Box>
     );
 }
 
